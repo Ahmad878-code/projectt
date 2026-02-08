0be63ad39787da75a3818633354bc7588cfc5558
Ahmad878-code <affan.grewal73@gmail.com>
2026-02-08 16:12:28 +0500

    main

---
+++ main.py
@@ -0,0 +1,28 @@
+# create  a tuple with different type
+tuplex = ("tuple", False,3.2 , 1)
+print(tuplex)
+
+
+#create a tuple
+tuplex = (4,6,2,3,8,1)
+print(tuplex)
+
+
+#tuples are immutable so you cannot add new elements
+# using merge of tuples with the + operator you can add elements and it will create a new tuple
+tuplex = tuplex + (9,)
+print(tuplex)
+
+#count the number of occurrences of items 50 from a tuple
+tuple1 = (50,10,60,70,50)
+print(tuple1.count(50))
+
+#create a tuple
+tuplex = (2,4,3,5,4,6,7,8,6,1)
+# use tuple(start:stop) the start index is inclusive and the stop index
+_slice = tuplex[3:5]\
+#is exclusive
+print(_slice)
+#if the start index isn't defined , it is taken from the beginning of the tuple
+_slice = tuplex[:6]
+print(_slice)
